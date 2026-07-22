fdf90c60e994e764f47b4d5312360b25e8f411d6
mhirrt12 <kaletamene90@gmail.com>
2026-07-22 06:05:33 -0700

    Fix incorrect cube value for 4 and add cubes for 6 and 7

--- python/hello.py
+++ python/hello.py
@@ -1,4 +1,4 @@
-cubes = [1, 8, 27, 65, 125]  # something's wrong here
-4 ** 3  # the cube of 4 is 64, not 65!
-cubes[3] = 64  # replace the wrong value
+cubes = [1, 8, 27, 64, 125]  # something's wrong here 
+cubes.append(216)  # add the cube of 6
+cubes.append(7 ** 3)  # and the cube of 7
 print(cubes)
