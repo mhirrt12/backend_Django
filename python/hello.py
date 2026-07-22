1678d6ff2400e769f94e34ec696a732aeaf774e6
mhirrt12 <kaletamene90@gmail.com>
2026-07-22 06:22:54 -0700

    Replace RGB object reference demonstration with integer input handling and conditional output

--- python/hello.py
+++ python/hello.py
@@ -1,6 +1,11 @@
-rgb = ["Red", "Green", "Blue"]
-rgba = rgb
-print(id(rgb) == id(rgba))  # they reference the same object
-
-rgba.append("Alph")
-print(rgb)
+x = int(input("Please enter an integer: "))
+#Please enter an integer: 42
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
